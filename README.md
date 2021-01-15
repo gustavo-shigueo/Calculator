c8b2dd3e4f48c148be199634b7203d950cc70a01
gustavo-shigueo <gustavo.gsmn@gmail.com>
2021-01-14 21:29:09 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Javascript Calculator
+
+I saw this calculator design on YouTube and decided to create something similar with several improvements to the JavaScript that controls the calculator's functions.
+
+The script.js file was completely written by me.
+
+The style.scss file is a SCSS version of the CSS code in the video linked below (with some alterations regarding the backspace and negative (+/-) buttons, which aren't present in the video).
+
+The style.min.css file is just the style.scss file compiled to minified CSS
+
+The vanilla-tilt.min.js file was downloaded from micku7zu's GitHub, link to the project can be founded below. 
+
+## Credits
+* Design (almost all of the CSS and HTML): https://www.youtube.com/watch?v=NhcZh8Bwr30&ab_channel=OnlineTutorials
+* Vanilla-tilt: https://micku7zu.github.io/vanilla-tilt.js/
+* Backspace SVG icon: https://fontawesome.com/icons/backspace?style=solid
---
+++ js/script.js
@@ -0,0 +1,59 @@
+const operators = document.querySelectorAll('.operator')
+const backspace = document.querySelector('.backspace')
+const negative = document.querySelector('.negative')
+const numbers = document.querySelectorAll('.num')
+const output = document.querySelector('.value')
+const clear = document.querySelector('.clear')
+const equal = document.querySelector('.equals')
+const point = document.querySelector('.point')
+
+const handleOperatorBtn = e => {
+  const [ op, val, len ] = [ e.target.getAttribute('data-operator'), output.value, output.value.length ]
+  if (val === '' || val === '.') output.value = `0 ${op} `
+  else if (val.match(/.+[\+\-\*\/]\s$/g)) output.value = `${val.substring(0, len - 2)}${op} `
+  else if (val.match(/.+\.$/g)) output.value = `${val.substring(0, len - 1)} ${op} `
+  else if (val !== '-') output.value += ` ${op} `
+}
+
+const handleNumberBtn = e => output.value += e.target.getAttribute('data-number')
+
+const handleBackspace = () => {
+  const amountToDelete = output.value[output.value.length - 1] !== ' ' ? 1 : 3
+  output.value = output.value.substring(0, output.value.length - amountToDelete)
+}
+
+const handleNegativeBtn = () => {
+  const { value } = output
+  const [ longRegex1, longRegex2 ] = [ /([\+\*\/\-]\s)(\d+|\d+\.\d?)$/g, /([\+\*\/\-]\s)(\-)(\d+|\d+\.\d?)$/g ]
+  if (value === '' || value.match(/^(\d+|\d+\.\d+)$/g)) output.value = `-${value}`
+  else if (value.replace(' ', '').match(/^\-\d{0,}(\.\d+)?$/)) output.value = value.substring(1)
+  else if (value.match(/[\+\*\/\-]\s$/g)) output.value += '-'
+  else if (value.match(/\-$/g)) handleBackspace()
+  else if (value.match(longRegex1)) output.value = value.replace(longRegex1, '$1-$2')
+  else if (value.match(longRegex2)) output.value = value.replace(longRegex2, '$1$3')
+}
+
+const handlePointBtn = () => {
+  if (output.value === '' || output.value.match(/.+[\+\-\*\/]$/g)) output.value += '0.'
+  else if (!output.value.match(/\d{0,}\.\d{0,}$/)) output.value += '.'
+}
+
+const handleEquals = () => {
+  try {
+    const result = eval(output.value).toString()
+    output.value = (result.split('.').length === 2 && result.split('.')[1].length > 5)
+      ? Number(result).toFixed(5)
+      : result
+  } catch (error) { return }
+}
+
+const handleClear = () => output.value = ''
+
+operators.forEach(operator => operator.addEventListener('click', e => handleOperatorBtn(e)))
+numbers.forEach(number => number.addEventListener('click', e => handleNumberBtn(e)))
+
+backspace.addEventListener('click', handleBackspace)
+negative.addEventListener('click', handleNegativeBtn)
+point.addEventListener('click', handlePointBtn)
+equal.addEventListener('click', handleEquals)
+clear.addEventListener('click', handleClear)
